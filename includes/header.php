f396947d6237ee4546de1d3126575c68c31e537d
Diego Ortega <juandiegoortega@unicomfacauca.edu.co>
2026-06-03 16:19:53 -0500

    Cambiar título

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ require_once $configPath;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Futbol Shopp</title>
+    <title>Futbol Shop</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
